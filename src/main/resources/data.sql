0ff85db9db7895f34e2dd0b4040d5cc0a6d64b6c
naulta <anault@ecsteam.com>
2018-08-04 16:45:42 -0600

    Added unit test. Preloaded Employee table, Fixed 404 response

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into Employee (id, date_of_birth, date_of_employment,first_name,last_name,middle_initial,status) VALUES (10000001,'06/28/1970','09/01/2018','Andy','Nault','H','ACTIVE');
+insert into Employee (id, date_of_birth, date_of_employment,first_name,last_name,middle_initial,status) VALUES (10000002,'06/28/1980','09/01/2018','Joey','Jones','H','ACTIVE');
+insert into Employee (id, date_of_birth, date_of_employment,first_name,last_name,middle_initial,status) VALUES (10000003,'06/28/1990','09/01/2018','Tim','Smith','H','ACTIVE');
+insert into Employee (id, date_of_birth, date_of_employment,first_name,last_name,middle_initial,status) VALUES (10000004,'06/28/2000','09/01/2018','Andy','Jones','H','ACTIVE');
+insert into Employee (id, date_of_birth, date_of_employment,first_name,last_name,middle_initial,status) VALUES (10000005,'06/28/1994','09/01/2018','Andy','Smith','H','ACTIVE');
