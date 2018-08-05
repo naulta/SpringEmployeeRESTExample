2c2772664740b04c9cb2c659dbb3a63e69909590
naulta <anault@ecsteam.com>
2018-08-04 21:33:21 -0600

    Fixed context issues with the unit tests and added additional tests

---
+++ src/test/java/com/kenzanexample/employeedemo/EmployeeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.kenzanexample.employeedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.kenzanexample.employeedemo.EmployeeApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=EmployeeApplication.class)
+public class EmployeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
